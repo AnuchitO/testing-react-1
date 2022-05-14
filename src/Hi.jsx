6a455b26ecbbe366ef746cf82333c07d66043b2d
AnuchitO <anuchit.prasertasng@gmail.com>
2022-05-14 11:09:47 +0700

    Option super hero

--- src/Hi.jsx
+++ src/Hi.jsx
@@ -5,6 +5,8 @@ class Hi extends React.Component {
     super(props)
     this.state = {
       superhero: '',
+      power: '',
+      optionPower: '',
     }
   }
 
@@ -12,7 +14,7 @@ class Hi extends React.Component {
     return (
       <div>
         <div>Hi {this.props.name}</div>
-        <label htmlFor="superhero"> Superhero Name: </label>
+        <label htmlFor="superhero"> Superhero Namex: </label>
         <input
           id="superhero"
           type="text"
@@ -22,7 +24,45 @@ class Hi extends React.Component {
           }}
         />
         <br />
-        <button onClick={() => alert('Hello ' + this.state.superhero)}>
+        <label htmlFor="power"> Superhero Power: </label>
+        <input
+          id="power"
+          type="text"
+          value={this.state.power}
+          onChange={(e) => {
+            this.setState({power: e.target.value})
+          }}
+        />
+
+        <br />
+        <select
+          value={this.state.optionPower}
+          onChange={(e) => {
+            this.setState({optionPower: e.target.value})
+          }}
+        >
+          <option value="">Select Power</option>
+          <option value="Flying">Flying</option>
+          <option value="Fire">Fire</option>
+          <option value="Water">Water</option>
+          <option value="Ice">Ice</option>
+          <option value="Lightning">Lightning</option>
+          <option value="Earth">Earth</option>
+          <option value="Wind">Wind</option>
+        </select>
+        <br />
+        <button
+          onClick={() =>
+            alert(
+              'Hello ' +
+                this.state.superhero +
+                ' power:' +
+                this.state.power +
+                ' ' +
+                this.state.optionPower
+            )
+          }
+        >
           Click me
         </button>
       </div>
