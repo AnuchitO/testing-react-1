b3fcc771cddf6e6b34f30c193f6b5ebc64c3edc2
AnuchitO <anuchit.prasertasng@gmail.com>
2022-05-21 14:41:22 +0700

    regx

--- src/example/testinglibrary/CounterUpDown.test.js
+++ src/example/testinglibrary/CounterUpDown.test.js
@@ -1,18 +1,30 @@
-import {fireEvent, render} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ExampleCounter from './CounterUpDown'
 
 test('should show default value', () => {
-  const {getByText} = render(<ExampleCounter />)
+  render(<ExampleCounter />)
 
-  expect(getByText('number: 0')).toBeInTheDocument()
+  expect(screen.getByText('number: 0')).toBeInTheDocument()
 })
 
 test('should show 1 when Click Count UP', () => {
-  const {getByText} = render(<ExampleCounter />)
+  render(<ExampleCounter />)
 
-  const button = getByText('Count UP')
+  const button = screen.getByText('Count UP')
   userEvent.click(button)
 
-  expect(getByText('number: 1')).toBeInTheDocument()
+  expect(screen.getByText('number: 1')).toBeInTheDocument()
+})
+
+test('counter up and down', () => {
+  render(<ExampleCounter />)
+  const up = screen.getByText(/Count UP/i)
+  userEvent.click(up)
+  expect(screen.getByText('number: 1')).toBeInTheDocument()
+
+  const down = screen.getByText(/Count DOWN/i)
+  userEvent.click(down)
+
+  expect(screen.getByText(/0/i)).toBeInTheDocument()
 })
