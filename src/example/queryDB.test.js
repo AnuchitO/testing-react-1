580f58282b384ddf4e35d437c1701f1d65e4ee35
AnuchitO <anuchit.prasertasng@gmail.com>
2022-05-21 11:03:19 +0700

    wrap error

--- src/example/queryDB.test.js
+++ src/example/queryDB.test.js
@@ -1,17 +1,36 @@
 import {queryDBReject} from './queryDB'
 
-test('queryDBReject', async () => {
-  await expect(queryDBReject()).rejects.toEqual({
-    message: 'connect ECONNREFUSED',
-  })
-})
+// test('queryDBReject', async () => {
+//   await expect(queryDBReject()).rejects.toEqual({
+//     message: 'connect ECONNREFUSED',
+//   })
+// })
 
 test('queryDBReject2', async () => {
   try {
     await queryDBReject()
   } catch (e) {
-    expect(e.message).toBe('connect ECONNREFUSED')
+    expect(e).toEqual({message: 'connect ECONNREFUSED'})
   }
 
   expect.assertions(1)
+})
+
+class NoErrorThrownError extends Error {}
+const getError = async (fn) => {
+  try {
+    await fn()
+
+    throw new NoErrorThrownError()
+  } catch (e) {
+    return e
+  }
+}
+
+test('use get error to handle error', async () => {
+  const err = await getError(() => queryDBReject())
+
+  expect(err).toEqual({
+    message: 'connect ECONNREFUSED',
+  })
 })
